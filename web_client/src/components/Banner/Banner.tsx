9df031175890c02753d2e6df56eea77cb0efd7a6
dangquochuy-student <2051120123@ut.edu.vn>
2024-11-03 13:38:57 +0700

    feat: add component Banner

---
+++ web_client/src/components/Banner/Banner.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React from 'react';
+import IntroCompany from '@/app/(page)/(home)/components/IntroCompany';
+
+import Header from '@/layout/Header/Header';
+
+const Banner = () => {
+    return (
+        <div
+            className="w-full pt-[600px] relative
+                        bg-[url('https://ongroup.com.vn/wp-content/uploads/2024/03/adobestock_308909064-1-1-%E2%80%93-Da-sua.png')] 
+                        bg-no-repeat bg-cover bg-center"
+        >
+            <Header />
+            <IntroCompany />
+        </div>
+    );
+};
+
+export default Banner;
