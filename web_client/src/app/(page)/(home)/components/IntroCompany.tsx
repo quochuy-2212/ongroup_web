e76884b72d2fec758c6d9d90917ef42bd78e708d
dangquochuy-student <2051120123@ut.edu.vn>
2024-11-03 13:37:19 +0700

    feat: add component IntroCompany

--- web_client/src/app/(page)/(home)/components/IntroCompany.tsx
+++ web_client/src/app/(page)/(home)/components/IntroCompany.tsx
@@ -3,30 +3,17 @@ import { intro_company_data } from '@/data/intro-company';
 
 const IntroCompany = () => {
     return (
-        <div>
-            <div className="container m-auto mt-16 w-full h-auto text-center p-10 space-y-5 ">
-                {/* <h2 className="uppercase text-5xl font-extrabold text-[var(--color-primary)]">
-                    {intro_company_data.name}
-                </h2> */}
-
-                <p className="text-[var(--text-color-secondary)] text-xl leading-10">
-                    {intro_company_data.description}
-                </p>
-                <div className="flex justify-center">
-                    <ImageTag
-                        src={intro_company_data.logo}
-                        alt="logo-company"
-                        className="bg-white p-4 w-64 rounded-2xl"
-                    />
-                </div>
-                {/* <div className="flex gap-10 justify-center">
-                    <button className="min-w-48 text-white font-bold bg-[var(--color-primary)] px-5 py-2 rounded-md">
-                        Về chúng tôi
-                    </button>
-                    <button className="min-w-48 text-white font-bold bg-[var(--color-primary)] px-5 py-2 rounded-md">
-                        Liên hệ
-                    </button>
-                </div> */}
+        <div className="w-full h-auto text-center space-y-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <p className="text-[var(--text-color-secondary)] text-xl leading-[80px]">
+                {intro_company_data.description}
+            </p>
+            <div className="flex justify-center">
+                <ImageTag src={intro_company_data.logo} alt="logo-company" className="bg-white p-4 w-64 rounded-2xl" />
+            </div>
+            <div className="flex gap-10 justify-center">
+                <button className="min-w-48 text-[var(--color-primary)] font-bold bg-white px-5 py-2 rounded-md">
+                    Về chúng tôi
+                </button>
             </div>
         </div>
     );
