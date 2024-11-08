b36ceb99352e312493760815e099037afe7d4393
dangquochuy-student <2051120123@ut.edu.vn>
2024-11-09 06:13:29 +0700

    fix: layout

--- web_client/src/app/(page)/layout.tsx
+++ web_client/src/app/(page)/layout.tsx
@@ -1,7 +1,6 @@
 import ButtonToTop from '@/components/ButtonToTop/ButtonToTop';
 import Banner from '@/layout/Banner/Banner';
 import Footer from '@/layout/Footer/Footer';
-import '@/global_style/style-posts.scss';
 
 export default function LayoutPage({
     children,
